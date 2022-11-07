fc0a48f7833b5d55a7f2cec1a12362614b621f39
fawaz <fawazfkp2@gmail.com>
2022-11-07 15:56:33 +0530

    crud operations

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from typing import Union
-
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
+
+dummydb= []
+
+class Data(BaseModel):
+    id:int
+    name:str
+    price:float
+    is_active:Optional[bool]=None
 
 
 @app.get("/")
 def read_root():
     return {"Welcome": "welcome"}
 
+@app.get("/datas/")
+def get_datas():
+    return dummydb
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/data/{data_id}")
+def get_data(data_id: int):
+    return dummydb[data_id]
+
+@app.post("/add-data")
+def add_data(data:Data):
+    dummydb.append(data.dict())
+    return dummydb[-1]
+
+@app.delete("/data-delete/{data_id}")
+def delete_data(data_id:int):
+    dummydb.pop(data_id)
+    return {"status":"deleted success"}
